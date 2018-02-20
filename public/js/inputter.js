c97afc3b5471c5de9598fa7861474a43eeab7978
Ashpan Raskar <ashpanr@gmail.com>
2018-02-19 19:02:19 -0500

    i give up this is so annoying

--- public/js/inputter.js
+++ public/js/inputter.js
@@ -4,145 +4,185 @@ var team = 0;
 
 var matchNumber = 0;
 
-function enterTeam(){
+// var counter = 0;
+function enterTeam() {
 
-  const teamText = document.getElementById('team-header');
-  team = document.getElementById('team').value;
-  teamText.innerText = "Current team: " + team;
+    const teamText = document.getElementById('team-header');
+    team = document.getElementById('team').value;
+    teamText.innerText = "Current team: " + team;
 
-  const matchnumberText = document.getElementById('matchnumber-header');
-  matchnumberText.innerText = "Match number: " + document.getElementById('matchnumber').value;
+    const matchnumberText = document.getElementById('matchnumber-header');
+    matchnumberText.innerText = "Match number: " + document.getElementById('matchnumber').value;
 
-  document.getElementById('team-input').style.display = "none";
-  document.getElementById('content-input').style.display = "";
+    document.getElementById('team-input').style.display = "none";
+    document.getElementById('content-input').style.display = "";
 
-  db.child(team + '/match-count').once('value').then(function(snap){
-    if (snap.val() != null){
-      matchNumber = snap.val() + 1;
-    }
-    else {
-      matchNumber = 1;
-    }
-  });
+    db.child(team + '/match-count').once('value').then(function(snap) {
+        if (snap.val() != null) {
+            matchNumber = snap.val() + 1;
+        } else {
+            matchNumber = 1;
+        }
+    });
 
 }
 
 function submitData() {
 
-  document.getElementById('content-input').style.display = "none";
-  document.getElementById('uploading-wait').style.display = "";
+    document.getElementById('content-input').style.display = "none";
+    document.getElementById('uploading-wait').style.display = "";
 
-  if (team == 0 || team == null || team == "") {
-    console.log("Team is null.");
-    failDataUpload('matchdata-loading', "Match data upload failed.");
-    failDataUpload('matchinfo-loading', "Match data upload failed.");
-    return;
-  }
-
-  db.child(team + '/match-count').once('value').then(function(snap){
-
-    if (snap.val() != null){
-      matchNumber = snap.val() + 1;
+    if (team == 0 || team == null || team == "") {
+        console.log("Team is null.");
+        failDataUpload('matchdata-loading', "Match data upload failed.");
+        failDataUpload('matchinfo-loading', "Match data upload failed.");
+        return;
     }
-    else {
-      matchNumber = 1;
-    }
-    updateDatabase();
 
-  });
+    db.child(team + '/match-count').once('value').then(function(snap) {
+
+        if (snap.val() != null) {
+            matchNumber = snap.val() + 1;
+        } else {
+            matchNumber = 1;
+        }
+        updateDatabase();
+
+    });
 
 }
 
 function finishReset() {
-  window.location.reload(false);
+    window.location.reload(false);
 }
 
 function failDataUpload(id, message) {
 
-  const dataAlert = document.getElementById(id);
-  dataAlert.setAttribute("class", "alert alert-danger");
-  dataAlert.innerText = message;
+    const dataAlert = document.getElementById(id);
+    dataAlert.setAttribute("class", "alert alert-danger");
+    dataAlert.innerText = message;
 
-  const icon = document.createElement('span');
-  icon.setAttribute("class", "glyphicon glyphicon-remove pull-right");
-  dataAlert.appendChild(icon);
+    const icon = document.createElement('span');
+    icon.setAttribute("class", "glyphicon glyphicon-remove pull-right");
+    dataAlert.appendChild(icon);
 
 }
 
 function successDataUpload(id, message) {
 
-  const dataAlert = document.getElementById(id);
-  dataAlert.setAttribute("class", "alert alert-success");
-  dataAlert.innerText = message;
+    const dataAlert = document.getElementById(id);
+    dataAlert.setAttribute("class", "alert alert-success");
+    dataAlert.innerText = message;
 
-  const icon = document.createElement('span');
-  icon.setAttribute("class", "glyphicon glyphicon-ok pull-right");
-  dataAlert.appendChild(icon);
+    const icon = document.createElement('span');
+    icon.setAttribute("class", "glyphicon glyphicon-ok pull-right");
+    dataAlert.appendChild(icon);
 
 }
 
 function updateDatabase() {
 
-  var matchString = 'match' + parseInt(matchNumber);
+    var matchString = 'match' + parseInt(matchNumber);
 
-  var updateCount = {};
-  updateCount['match-count'] = matchNumber;
-  db.child(team).update(updateCount);
+    var updateCount = {};
+    updateCount['match-count'] = matchNumber;
+    db.child(team).update(updateCount);
 
-  try {
+    try {
 
-    db.child (team + '/matches/' + matchString).set({
-      starting_position: document.getElementById('position').value,
-      auto_baseline: document.querySelector('input[name="baseline"]:checked').value,
-      auto_switch: document.querySelector('input[name="auto_switch"]:checked').value,
-      auto_scale: document.querySelector('input[name="auto_scale"]:checked').value,
-      teleop_switch_success: document.getElementById('teleop_switch_success').value,
-      teleop_switch_fail: document.getElementById('teleop_switch_fail').value,
-      teleop_scale_success: document.getElementById('teleop_scale_success').value,
-      teleop_scale_fail: document.getElementById('teleop_scale_fail').value,
-      teleop_opp_switch_success: document.getElementById('teleop_opp_switch_success').value,
-      teleop_opp_switch_fail: document.getElementById('teleop_opp_switch_fail').value,
-      teleop_vault: document.getElementById('teleop_vault').value,
-      offense: document.getElementById('offense').checked,
-      defense: document.getElementById('defense').checked,
-      climb: document.getElementById('climb').value,
-      boost: document.getElementById('boost').checked,
-      force: document.getElementById('force').checked,
-      levitate: document.getElementById('levitate').checked,
-      powerup_times: document.getElementById('powerup_time').value,
-      disconnects: document.getElementById('dcs').value,
-      comment: document.getElementById('comment').value
-    }).then(function(done){
-      console.log("Successfully uploaded data to allteams/" + team + "/matches/" + matchString);
-      successDataUpload('matchdata-loading', "Match data upload succeeded!");
+        db.child(team + '/matches/' + matchString).set({
+            starting_position: document.getElementById('position').value,
+            auto_baseline: document.querySelector('input[name="baseline"]:checked').value,
+            auto_switch: document.querySelector('input[name="auto_switch"]:checked').value,
+            auto_scale: document.querySelector('input[name="auto_scale"]:checked').value,
+            teleop_switch_success: document.getElementById('teleop_switch_success').value,
+            teleop_switch_fail: document.getElementById('teleop_switch_fail').value,
+            teleop_scale_success: document.getElementById('teleop_scale_success').value,
+            teleop_scale_fail: document.getElementById('teleop_scale_fail').value,
+            teleop_opp_switch_success: document.getElementById('teleop_opp_switch_success').value,
+            teleop_opp_switch_fail: document.getElementById('teleop_opp_switch_fail').value,
+            teleop_vault: document.getElementById('teleop_vault').value,
+            offense: document.getElementById('offense').checked,
+            defense: document.getElementById('defense').checked,
+            climb: document.querySelector('input[name="climb"]:checked').value,
+            boost: document.getElementById('boost').checked,
+            force: document.getElementById('force').checked,
+            levitate: document.getElementById('levitate').checked,
+            powerup_times: document.getElementById('powerup_time').value,
+            disconnects: document.getElementById('dcs').value,
+            team_number: document.getElementById('team').value,
+            match_number: document.getElementById('matchnumber').value,
+            scouter: document.getElementById('scouter').value,
+            comment: document.getElementById('comment').value
+        }).then(function(done) {
+            console.log("Successfully uploaded data to allteams/" + team + "/matches/" + matchString);
+            successDataUpload('matchdata-loading', "Match data upload succeeded!");
+        });
+
+    } catch (err) {
+        failDataUpload('matchdata-loading', "Match data upload failed.");
+        console.log("Match data upload failed /matches/");
+    }
+
+    if (document.getElementById('comment').value == "" || document.getElementById('comment').value == null) {
+        document.getElementById('comment').value = "-";
+    }
+
+    try {
+        db.child(team + '/matches-info/' + matchString).set({
+            number: document.getElementById('matchnumber').value,
+            scouter: document.getElementById('scouter').value,
+            comment: document.getElementById('comment').value
+        }).then(function(done) {
+            console.log("Successfully uploaded data to allteams/" + team + "/matches-info/" + matchString);
+            successDataUpload('matchinfo-loading', "Match info upload succeeded!");
+        });
+    } catch (err) {
+        failDataUpload('matchinfo-loading', "Match data upload failed.");
+        console.log("Match data upload failed /matches-info/");
+    }
+
+    firebase.database().ref().child('teamlist/' + team).set(1);
+
+    console.log("Team " + team + " added to teamlist.");
+
+    var valKey = []
+    var query = firebase.database().ref("rawdata").orderByKey();
+    query.once("value").then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            valKey.push(key);
+            console.log("up: " + valKey)
+        });
+        // key = parseInt(key) + 1;
     });
 
-  }
-  catch (err) {
-    failDataUpload('matchdata-loading', "Match data upload failed.");
-  }
-
-  if (document.getElementById('comment').value == "" || document.getElementById('comment').value == null) {
-    document.getElementById('comment').value = "-";
-  }
-
-  try {
-    db.child (team + '/matches-info/' + matchString).set({
-      number: document.getElementById('matchnumber').value,
-      scouter: document.getElementById('scouter').value,
-      comment: document.getElementById('comment').value
-    }).then(function(done){
-      console.log("Successfully uploaded data to allteams/" + team + "/matches-info/" + matchString);
-      successDataUpload('matchinfo-loading', "Match info upload succeeded!");
-    });
-  }
-  catch (err) {
-    failDataUpload('matchinfo-loading', "Match data upload failed.");
-  }
-
-  firebase.database().ref().child('teamlist/' + team).set(1);
-  console.log("Team " + team + " added to teamlist.");
-
+    var key = valKey.slice(-1)[0];
+    console.log("key: " + valKey);
+    firebase.database().ref().child('rawdata/' + key).set({
+        starting_position: document.getElementById('position').value,
+        auto_baseline: document.querySelector('input[name="baseline"]:checked').value,
+        auto_switch: document.querySelector('input[name="auto_switch"]:checked').value,
+        auto_scale: document.querySelector('input[name="auto_scale"]:checked').value,
+        teleop_switch_success: document.getElementById('teleop_switch_success').value,
+        teleop_switch_fail: document.getElementById('teleop_switch_fail').value,
+        teleop_scale_success: document.getElementById('teleop_scale_success').value,
+        teleop_scale_fail: document.getElementById('teleop_scale_fail').value,
+        teleop_opp_switch_success: document.getElementById('teleop_opp_switch_success').value,
+        teleop_opp_switch_fail: document.getElementById('teleop_opp_switch_fail').value,
+        teleop_vault: document.getElementById('teleop_vault').value,
+        offense: document.getElementById('offense').checked,
+        defense: document.getElementById('defense').checked,
+        climb: document.querySelector('input[name="climb"]:checked').value,
+        boost: document.getElementById('boost').checked,
+        force: document.getElementById('force').checked,
+        levitate: document.getElementById('levitate').checked,
+        powerup_times: document.getElementById('powerup_time').value,
+        disconnects: document.getElementById('dcs').value,
+        team_number: document.getElementById('team').value,
+        match_number: document.getElementById('matchnumber').value,
+        scouter: document.getElementById('scouter').value,
+        comment: document.getElementById('comment').value
+    })
 }
-
 //document.querySelector('input[name="genderS"]:checked').value;
