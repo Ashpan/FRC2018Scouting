ea34296c53a13d0e01a76acda269a5992f412307
Ashpan Raskar <ashpanr@gmail.com>
2018-04-05 19:21:35 -0400

    ready for mac i think

--- public/js/init.js
+++ public/js/init.js
@@ -1,3 +1,40 @@
+// Initialize YORK DATABASE
+//
+// var config = {
+//     apiKey: "AIzaSyCxahRnzexvO_C-K6hFxgFb8Jvavr95Ick",
+//     authDomain: "scouting-compiler-onnyo.firebaseapp.com",
+//     databaseURL: "https://scouting-compiler-onnyo.firebaseio.com",
+//     projectId: "scouting-compiler-onnyo",
+//     storageBucket: "scouting-compiler-onnyo.appspot.com",
+//     messagingSenderId: "577025368281"
+// };
+// firebase.initializeApp(config);
+
+// Initialize MAC DATABASE 
+var config = {
+    apiKey: "AIzaSyCDKw1Cf34z_5JV-ZuyEL1O8_JpSUWxUj8",
+    authDomain: "scouting-compiler-onham.firebaseapp.com",
+    databaseURL: "https://scouting-compiler-onham.firebaseio.com",
+    projectId: "scouting-compiler-onham",
+    storageBucket: "scouting-compiler-onham.appspot.com",
+    messagingSenderId: "324647744751"
+};
+firebase.initializeApp(config);
+
+// Initialize DEV DATABASE 
+// var config = {
+//     apiKey: "AIzaSyAUxp-Hz_5ZKo2WabIiEU3PUU8TEGwWAQE",
+//     authDomain: "scouting-compiler-dev.firebaseapp.com",
+//     databaseURL: "https://scouting-compiler-dev.firebaseio.com",
+//     projectId: "scouting-compiler-dev",
+//     storageBucket: "scouting-compiler-dev.appspot.com",
+//     messagingSenderId: "211665111326"
+// };
+// firebase.initializeApp(config);
+
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
 var whitelisted_emails = []
 
 
@@ -22,6 +59,7 @@ function google_signin() {
     var user = firebase.auth().currentUser;
     if (user) {
         loginStatus();
+        alert('You\'re already signed in');
     } else {
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
@@ -36,6 +74,10 @@ function google_signin() {
             var errorMessage = error.message;
             var email = error.email;
             var credential = error.credential;
+            console.log(errorCode);
+            console.log(errorMessage);
+            console.log(email);
+            console.log(credential);
             // ...
         });
 
@@ -54,6 +96,7 @@ function loginStatus() {
         updateNav();
         // document.getElementById("login_link").onclick = google_signout();
     } else {
+
         // document.getElementById("login_link").onclick = google_signin();
 
     }
@@ -93,5 +136,7 @@ window.onload = function() {
 
 // Disable scroll
 $(function() {
-    $(':input[type=number]').on('mousewheel', function(e) { $(this).blur(); });
+    $(':input[type=number]').on('mousewheel', function(e) {
+        $(this).blur();
+    });
 });
