b4951d50806d58012da16679c4f95429c56fa205
WilliamLQin <william.qin51@gmail.com>
2018-04-06 14:29:19 -0400

    fix bugs

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -40,6 +40,9 @@ function analyzeData() {
     }
 
     var i = data.overall_teleop_success.length - mostRecent;
+    if (i < 0) {
+        i = 0;
+    }
     overall_teleop_success.forEach(val => {
         if (val > 0) {
             teleop_switch_success.push(data.teleop_switch_success[i]);
@@ -51,6 +54,9 @@ function analyzeData() {
     });
 
     var j = data.overall_auto_success.length - mostRecent;
+    if (j < 0) {
+        j = 0;
+    }
     overall_auto_success.forEach(val => {
         if (val > 0) {
             auto_switch_success.push(data.auto_switch_success[j]);
@@ -97,4 +103,84 @@ function pushToStats(team) {
             $("#uploading").hide();
         }, 750);
     });
+}
+
+function createGraph() {
+
+    var data = [
+        {month: 'Jan', A: 20, B: 5, C: 10},
+        {month: 'Feb', A: 30, B: 10, C: 20}
+    ];
+     
+    var xData = ["A", "B", "C"];
+     
+    var margin = {top: 20, right: 50, bottom: 30, left: 50},
+            width = 400 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+     
+    var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .35);
+     
+    var y = d3.scale.linear()
+            .rangeRound([height, 0]);
+     
+    var color = d3.scale.category20();
+     
+    var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+     
+    var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+     
+    var dataIntermediate = xData.map(function (c) {
+        return data.map(function (d) {
+            return {x: d.month, y: d[c]};
+        });
+    });
+     
+    var dataStackLayout = d3.layout.stack()(dataIntermediate);
+     
+    x.domain(dataStackLayout[0].map(function (d) {
+        return d.x;
+    }));
+     
+    y.domain([0,
+        d3.max(dataStackLayout[dataStackLayout.length - 1],
+                function (d) { return d.y0 + d.y;})
+        ])
+      .nice();
+     
+    var layer = svg.selectAll(".stack")
+            .data(dataStackLayout)
+            .enter().append("g")
+            .attr("class", "stack")
+            .style("fill", function (d, i) {
+                return color(i);
+            });
+     
+    layer.selectAll("rect")
+            .data(function (d) {
+                return d;
+            })
+            .enter().append("rect")
+            .attr("x", function (d) {
+                return x(d.x);
+            })
+            .attr("y", function (d) {
+                return y(d.y + d.y0);
+            })
+            .attr("height", function (d) {
+                return y(d.y0) - y(d.y + d.y0);
+            })
+            .attr("width", x.rangeBand());
+     
+    svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+            
 }
